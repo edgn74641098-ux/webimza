d0ebff48f09877cdf82e9d882e9053db47412938
ErkanD59 <162594282+ErkanD59@users.noreply.github.com>
2026-05-17 13:13:26 +0300

    Render action dropdown as floating fixed menu

--- backend/resources/views/components/ui/dropdown.blade.php
+++ backend/resources/views/components/ui/dropdown.blade.php
@@ -1,4 +1,34 @@
-<div x-data="{ open:false }" class="relative">
-  <div @click="open=!open">{{ $trigger }}</div>
-  <div x-show="open" @click.outside="open=false" class="absolute right-0 z-30 mt-2 w-44 rounded-lg border border-slate-200 bg-white p-1 shadow">{{ $slot }}</div>
+<div
+    x-data="{
+        open: false,
+        top: 0,
+        left: 0,
+        width: 176,
+        place() {
+            const rect = this.$refs.trigger.getBoundingClientRect();
+            this.top = rect.bottom + 8;
+            this.left = Math.max(8, rect.right - this.width);
+        },
+        toggle() {
+            this.open = !this.open;
+            if (this.open) this.$nextTick(() => this.place());
+        }
+    }"
+    @keydown.escape.window="open=false"
+    @scroll.window="if (open) place()"
+    @resize.window="if (open) place()"
+    class="inline-block"
+>
+    <div x-ref="trigger" @click="toggle()">{{ $trigger }}</div>
+
+    <div
+        x-show="open"
+        x-cloak
+        @click.outside="open=false"
+        x-transition.opacity
+        class="fixed z-50 w-44 rounded-lg border border-slate-200 bg-white p-1 shadow"
+        :style="`top:${top}px;left:${left}px`"
+    >
+        {{ $slot }}
+    </div>
 </div>
